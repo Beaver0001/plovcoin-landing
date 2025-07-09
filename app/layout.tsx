1aa888adf00fdca11ee0fe3b4cf89c5f554eb682
Beaver0001 <mkhalifaev@gmail.com>
2025-07-10 03:05:02 +0500

    Fixed children type in app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "PlovCoin",
-  description: "The tastiest meme coin on Solana",
+  title: 'PlovCoin',
+  description: 'The tastiest meme coin on Solana',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
